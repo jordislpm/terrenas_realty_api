b9f946c14eb4a038cdfe187db767c626bc42e63c
jordis.dev <jordislpm@gmail.com>
2025-06-14 14:56:27 -0400

    improve the cookie for token

--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -1,10 +1,6 @@
-
-
 import { Request, Response, Router } from "express";
 import { loginUser } from "../../business-logic/auth/login";
 import { loginUserDTO } from "../../entities/user/user.dto";
-
-
 
 const routerLogin: Router = Router();
 
@@ -12,24 +8,26 @@ routerLogin.post("/login", async (req: Request, res: Response) => {
   const { body } = req;
   const userToLogin = body as loginUserDTO;
   try {
-    const userValidated = await loginUser({data: userToLogin});
-    const {token, age, user} = userValidated
+    const userValidated = await loginUser({ data: userToLogin });
+    const { token, age, user } = userValidated;
 
-  const {password,...userInfo}= user;
+    const { password, ...userInfo } = user;
 
-    console.log(userInfo)
+    console.log(userInfo);
     res
-    .cookie("token", token,{
-        httpOnly:true,
-        secure:true ,
-        maxAge: age
-    })
-    .status(200)
-    .json(userInfo)
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
   } catch (error) {
-
     console.error("Error:", error);
-    res.status(500).json({ error: `Error in server, user not validated: ${error}` });
+    res
+      .status(500)
+      .json({ error: `Error in server, user not validated: ${error}` });
   }
 });
 
